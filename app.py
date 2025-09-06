5b3429f59f28c801e9046d942baaebbff911de33
Egor Ulybin <egorulybin@gmail.com>
2025-09-06 13:10:26 +0200

    basic api structure set up and tested with insomnia

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+from flask import Flask, request
 
+app = Flask(__name__)
+
+
+@app.route("/todos")
+def getTodos():
+    return "Here is a list of all todos."
+
+
+@app.route("/todos/addTodo", methods=["POST"])
+def addTodo():
+    return "Todo was added!"
+
+
+@app.route("/todos/deleteTodo/<position>", methods=["DELETE"])
+def deleteTodo(position):
+    return "Todo to delete is: " + position
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3245, debug=True)
