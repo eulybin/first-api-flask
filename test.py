2e6abdda2bdc8981fc874a78e5c1c095c2619e9e
Egor Ulybin <egorulybin@gmail.com>
2025-09-13 18:02:25 +0200

    using api key for weather api

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 import requests
 
-res = requests.get("http://www.official-joke-api.appspot.com/random_joke")
-print(res.json()["punchline"])
+API_KEY = "bc02ae346739ccbf63347d4934ae8885"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+parameters = {"q": "Madrid", "appid": API_KEY}
+
+res = requests.get(BASE_URL, params=parameters)
+res.raise_for_status()
+print(res.json())
