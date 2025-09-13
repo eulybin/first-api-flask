bdb2b24e8d966cf6120c0aed517697716a53b42c
Egor Ulybin <egorulybin@gmail.com>
2025-09-13 18:37:22 +0200

    using environmental variables to store api key securely

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import requests
+import os
 
-API_KEY = "bc02ae346739ccbf63347d4934ae8885"
+API_KEY = os.environ.get("owm_api_key")
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 parameters = {"q": "Madrid", "appid": API_KEY}
 
 res = requests.get(BASE_URL, params=parameters)
 res.raise_for_status()
+print(API_KEY)
 print(res.json())
